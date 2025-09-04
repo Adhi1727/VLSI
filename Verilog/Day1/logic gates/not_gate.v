edc2562b9f72ecc9f37384d319d27270b3618f90
Adhi1727 <adhik8108@gmail.com>
2025-09-04 10:23:17 +0530

    Update not_gate.v

--- Verilog/Day1/logic gates/not_gate.v
+++ Verilog/Day1/logic gates/not_gate.v
@@ -1,1 +1,44 @@
+//Gate level
+module not_gate(a,b);
+  input a;
+  output b;
+  not(a,b);
+endmodule
 
+// behavioural level
+module not_gate(y,a);
+  input a;
+  output reg y;
+  always @(a)
+   y=~a;
+
+  
+//dataflow
+module not_gate(a,b);
+input a;
+output b;
+assign b=~a;
+endmodule
+
+
+
+//Testbech 
+module not_gate_tb;
+  reg a;
+  wire b;
+ not_gate dut (
+    .b(b),
+    .a(a)
+  );
+
+  initial begin
+    $display("Time\t a\t b");
+    $monitor("%0t\t %b\t %b", $time, a, b);
+
+    a = 0; #10;
+    a = 1; #10;
+
+
+    $finish;
+  end
+endmodule
