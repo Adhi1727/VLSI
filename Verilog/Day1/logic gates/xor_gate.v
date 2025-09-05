5ec30dc7e4a73622376ee4cc6c235f056562870e
Adhi1727 <adhik8108@gmail.com>
2025-09-05 09:53:02 +0530

    Update xor_gate.v

--- Verilog/Day1/logic gates/xor_gate.v
+++ Verilog/Day1/logic gates/xor_gate.v
@@ -4,3 +4,42 @@ input a,b;
 output o;
 assign o=a^b;
 endmodule
+
+//gatelevel
+module xor_gate(o,a,b);
+  input a.b;
+  output o;
+  xor(o,a,b);
+endmodule
+
+// behavioural level
+module xor_gate(o,a,b);
+  input a,b;
+  output reg o;
+  always @(a,b)
+  o=a^b; 
+endmodule
+
+
+//testbench
+module tb_xor;
+
+  reg a, b;
+  wire o;
+
+  xor_gate uut (.a(a), .b(b), .o(o));
+
+  initial begin
+    $dumpfile("xor_wave.vcd");
+    $dumpvars(0, tb_xor);
+
+    $display("time | a b | o");
+    $monitor("%4t | %b %b | %b", $time, a, b, o);
+
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+    $finish;
+  end
+endmodule
