c8ff8ad09e8e4c6f38fb2d80856bbc70fbf75b54
Adhi1727 <adhik8108@gmail.com>
2025-09-09 14:33:26 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/Assignment1/assignment3/encoder4x2/tb_encoder_2x4.v
@@ -0,0 +1,19 @@
+module tb_encoder_4x2();
+reg [3:0]a;
+wire [1:0]y;
+
+encoder_4x2 dut(a,y);
+
+initial begin
+
+$monitor("a=%b y=%b",a,y);
+
+a=4'b0001;#10
+a=4'b0010;#10
+a=4'b0100;#10
+a=4'b1000;#10
+a=4'b1100;#10
+
+$finish;
+end 
+endmodule
