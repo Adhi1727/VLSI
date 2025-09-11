fd43d6ea2ea70933095cc78235a240e8eacd2dcf
Adhi1727 <adhik8108@gmail.com>
2025-09-11 14:46:45 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/assignment5/rc_qns_22/immediate.v
@@ -0,0 +1,20 @@
+module immediate;
+reg clk;
+reg a,b;
+
+        initial begin
+                clk=0;
+        forever #5 clk=~clk;
+        end
+
+        initial begin
+        a=0;b=1;
+        $monitor("time=%0t,clk=%b,a=%b,b=%b",$time,clk,a,b);
+        #20 $finish;
+        end
+
+         always @(posedge clk) begin
+                a = b;
+                b = a;
+        end
+endmodule
