0526bfe65986e5184d5df0e6ff3855b45c0ef783
Adhi1727 <adhik8108@gmail.com>
2025-09-16 11:05:58 +0530

    Add files via upload

---
+++ Verilog/day3/behavioral/and_gate.v/tb_and_gate.v
@@ -0,0 +1,18 @@
+module tb_and_gate;
+reg a,b;
+wire y;
+
+and_gate uut(.a(a),.b(b),.y(y));
+
+
+	initial begin
+	$display(" time | a b | y ");
+	$monitor("%0t | %b %b | %b",$time,a,b,y);
+
+	a=0; b=0;#10;
+	a=0; b=1;#10;
+	a=1; b=0;#10;
+	a=1; b=1;#10;
+	$finish;
+	end
+endmodule
