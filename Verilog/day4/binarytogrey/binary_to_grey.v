ea908de27f47311eb1baeea6ad0f283f38241c77
Adhi1727 <adhik8108@gmail.com>
2025-09-16 13:12:37 +0530

    Add files via upload

---
+++ Verilog/day4/binarytogrey/binary_to_grey.v
@@ -0,0 +1,10 @@
+module binary_to_grey(
+input [3:0] b,
+output [3:0] g
+);
+
+	assign g[3:0]= {b[3]?1'b1:1'b0,
+               (b[3]^b[2])?1'b1:1'b0,
+               (b[2]^b[1])?1'b1:1'b0,
+               (b[1]^b[0])?1'b1:1'b0};
+endmodule
