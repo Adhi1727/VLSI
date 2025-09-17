21b72c03425b2a2f4b48b9f889db32cafa33ebae
Adhi1727 <adhik8108@gmail.com>
2025-09-17 16:21:08 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/assignment7/01.Tasks/04.Square_8bit.v/square_8bit.v
@@ -0,0 +1,22 @@
+module square_8bit;
+reg [7:0] a;
+wire [15:0] sqr;
+
+reg [15:0] out;
+
+initial begin
+	a=8'd8;
+
+	bit8_sqr(a,out);
+	$display("a=%0d --> sqr=%0d",a,out);
+end
+
+	task bit8_sqr;
+	input [7:0] x;
+	output [15:0] out;
+
+	begin
+		out = x*x;
+	end
+	endtask
+endmodule
