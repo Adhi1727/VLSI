33bd7408b308a4e76f13bdb7458fd2563da8a67b
Adhi1727 <adhik8108@gmail.com>
2025-09-21 14:37:35 +0530

    Add files via upload

---
+++ Verilog/day6/05.Toggle task.v/toggle.v
@@ -0,0 +1,23 @@
+module toggle;
+reg sig;
+
+	task toggle; 	
+	   inout sig;
+		begin
+	         #10 sig = ~sig;
+           end
+	endtask
+
+	initial begin
+		sig = 0;
+
+	$monitor("time=%0t,sig=%b",$time,sig);
+
+
+	repeat (10) begin
+		toggle(sig);
+	end
+	#10 $finish;
+	end
+endmodule
+
